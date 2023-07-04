405226e861c6911636ad0e3c4fa5e32a87cf48c4
Волынка <olganv1707@yandex.ru>
2023-07-05 02:11:19 +0300

    feat: создание структуры для shareit

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,5 +1,7 @@
 package ru.practicum.shareit.item.dto;
+
 import lombok.Data;
+
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
@@ -16,7 +18,7 @@ public class ItemDto {
     private String description;
     @NotNull
     private Boolean available;
-    
+
     public ItemDto(String name, String description, Boolean available) {
 
         this.name = name;
