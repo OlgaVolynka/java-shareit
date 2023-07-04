0a6c620058af19f4c382ba05f8d9357cc01feafd
Волынка <olganv1707@yandex.ru>
2023-07-05 00:42:00 +0300

    feat: создание структуры для shareit

---
+++ src/main/java/ru/practicum/shareit/exeption/DataNotFoundException.java
@@ -0,0 +1,8 @@
+package ru.practicum.shareit.exeption;
+
+public class DataNotFoundException extends RuntimeException{
+
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+}
