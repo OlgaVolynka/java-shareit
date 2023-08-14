a9bbcc91ab8ab7a70e2911e8f0eff136a0bb6058
Волынка <olganv1707@yandex.ru>
2023-08-14 18:04:40 +0300

    докер

--- server/src/main/java/ru/practicum/shareit/ShareItServer.java
+++ server/src/main/java/ru/practicum/shareit/ShareItServer.java
@@ -1,16 +1,4 @@
-/*package ru.practicum.shareit;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class ShareItServer {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ShareItServer.class, args);
-	}
-
-}*/
 package ru.practicum.shareit;
 
 import org.springframework.boot.SpringApplication;
@@ -21,11 +9,11 @@ import java.util.Collections;
 @SpringBootApplication
 public class ShareItServer {
 
-	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(ShareItServer.class);
-		app.setDefaultProperties(Collections
-				.singletonMap("server.port", "9090"));
-		app.run(args);
-	}
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(ShareItServer.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "9090"));
+        app.run(args);
+    }
 
 }
--- server/src/main/java/ru/practicum/shareit/booking/model/Status.java
+++ server/src/main/java/ru/practicum/shareit/booking/model/Status.java
@@ -1,14 +1,5 @@
 package ru.practicum.shareit.booking.model;
 
-import lombok.Getter;
-import lombok.Setter;
-
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "bookings", schema = "public")
-
 public enum Status {
     WAITING,
     APPROVED,
