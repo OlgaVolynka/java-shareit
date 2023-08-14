d5cd41a9a74fc380adc5d6ac4b7d31531839fb0d
Волынка <olganv1707@yandex.ru>
2023-08-14 09:38:07 +0300

    feat: разделение на 2 микросервиса

--- gateway/src/main/java/ru/practicum/shareitgateway/item/Comments.java
+++ gateway/src/main/java/ru/practicum/shareitgateway/item/Comments.java
@@ -3,34 +3,28 @@ package ru.practicum.shareitgateway.item;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
-import static javax.persistence.GenerationType.IDENTITY;
 
-@Entity
-@Table(name = "comments", schema = "public")
 @Getter
 @Setter
 public class Comments {
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
-    @Column(name = "id")
+
     private Long id;
-    @Column(name = "text")
+
     @NotEmpty
     private String text;
-    @Column(name = "item_Id")
+
 
     private Long itemId;
-    @Column(name = "author_Id")
+
 
     private Long authorId;
-    @Column(name = "created_time")
+
     private LocalDateTime created = LocalDateTime.now();
-    @Column(name = "author_Name")
+
     private String authorName;
 
     @Override
