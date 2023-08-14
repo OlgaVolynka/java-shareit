cadc90ae681c82502f705f59bd0fe27ef256f8e6
Волынка <olganv1707@yandex.ru>
2023-08-14 13:43:07 +0300

    докер

--- server/src/main/java/ru/practicum/shareit/booking/model/Status.java
+++ server/src/main/java/ru/practicum/shareit/booking/model/Status.java
@@ -1,5 +1,14 @@
 package ru.practicum.shareit.booking.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "bookings", schema = "public")
+
 public enum Status {
     WAITING,
     APPROVED,
